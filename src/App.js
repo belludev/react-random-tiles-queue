bcff028a583f2008a5922ee193d434a40d43d4b8
Andrew Bellucci <andrewbellucci@Andrews-MBP-2.fios-router.home>
2019-10-03 10:57:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      // These would be the cards with their IDs associated and the currentCards/cardQueue would be referencing the IDs
+      cards: [1,2,3,4,5,6],
+      currentCards: [1,2,3,4],
+      cardQueue: [5,6]
+    }
+  }
+  componentDidMount(){
+    this.cardsCycle = setInterval(
+      () => this.nextCardInQueue(),
+      3500
+    )
+  }
+  nextCardInQueue = () => {
+    let getRandomInt = (min, max) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+    let randCard = getRandomInt(0, this.state.currentCards.length);
+    let cardQueue = [...this.state.cardQueue]
+    let currentCards = [...this.state.currentCards];
+    let cardHold = currentCards[randCard];
+    currentCards[randCard] = cardQueue[0];
+    cardQueue.shift();
+    cardQueue.push(cardHold);
+    this.setState({
+      currentCards,
+      cardQueue
+    });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.currentCards.map(card => <div className="card" key={card}>card {card}</div>)}
+        Current Card Queue: {this.state.cardQueue.map(card => `${card} `)}
+      </div>
+    )
+  }
 }
-
-export default App;
